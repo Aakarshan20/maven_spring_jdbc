03859da27c990eff856e3607ee684c92a150e4eb
TomChiu <iiooiio@hotmail.com>
2020-02-23 18:06:54 +0800

    解耦成功(案例1)

--- src/main/java/com/it/jdbc/JdbcDemo1.java
+++ src/main/java/com/it/jdbc/JdbcDemo1.java
@@ -2,6 +2,19 @@ package com.it.jdbc;
 
 /*
 * 程序的耦合
+* 耦合:
+*    程序間的依賴
+*   方法間的依賴
+* 解耦:
+*   降低程序間的一爛
+* 開發中:
+*   編譯期不依賴 運行時不依賴
+* 解決:
+*   使用反射Class.forName來創建對象 避免使用new
+*   缺點:
+*       com.mysql.jdbc.Driver寫死 此處不OK
+* 解決2:
+*   透過讀取配置類，來獲取要創建的對象類名  
 * */
 
 import java.sql.*;
@@ -9,7 +22,10 @@ import java.sql.*;
 public class JdbcDemo1 {
     public static void main(String[] args) throws Exception{
         //1.註冊驅動
-        DriverManager.registerDriver( new com.mysql.jdbc.Driver());
+        //DriverManager.registerDriver( new com.mysql.jdbc.Driver());
+        //解耦
+        Class.forName("com.mysql.jdbc.Driver");
+
         //2.獲取連接
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/eesy", "root", "1234");
         //3.取得操作DB的預處裡對象
