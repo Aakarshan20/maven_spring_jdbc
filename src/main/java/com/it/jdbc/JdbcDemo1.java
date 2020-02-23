90dccb2156b2064b8ef6857b7e2f2caec614d329
TomChiu <iiooiio@hotmail.com>
2020-02-23 16:49:14 +0800

    first commit

---
+++ src/main/java/com/it/jdbc/JdbcDemo1.java
@@ -0,0 +1,14 @@
+package com.it.jdbc;
+
+/*
+* 程序的耦合
+* */
+
+public class JdbcDemo1 {
+    public static void main(String[] args) {
+        //1.註冊驅動
+        //2.獲取連接
+        //3.取得操作DB的預處裡對象
+        //4.執行sql 得到結果集
+    }
+}
